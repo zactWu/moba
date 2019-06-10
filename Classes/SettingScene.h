978ca4016f960b91fac6182ebc204c516a94afbd
Zact <1756202480@qq.com>
2019-06-10 19:38:26 +0800

    修改SettingScene

--- Classes/SettingScene.h
+++ Classes/SettingScene.h
@@ -8,6 +8,9 @@
 
 #include <stdio.h>
 #include "cocos2d.h"
+#include "ui/CocosGUI.h"
+
+using namespace cocos2d::ui;
 
 class SettingScene : public cocos2d::Layer
 {
@@ -15,6 +18,9 @@ public:
     static cocos2d::Scene* createScene();
     virtual bool init();
     void menuBackCallback(cocos2d::Ref* pSender);
+    CheckBox* checkbox[3] = {nullptr};
+    void addCheckBox();
+    void CheckButtontouchEvent(CheckBox* sender, CheckBox::EventType type);
     cocos2d::Menu* createText();
     cocos2d::Sprite* createBGImage();
     CREATE_FUNC(SettingScene);
