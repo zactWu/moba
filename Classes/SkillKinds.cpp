b6b514221311aa89db730dd4002835947ca2f1b4
Zact <1756202480@qq.com>
2019-06-16 19:47:12 +0800

    删去多余文件

--- Classes/SkillKinds.cpp
+++
@@ -1,6 +0,0 @@
-#include "cocos2d.h"
-#include "UnitClass.h"
-#include "skillClass.h"
-#include "GameScene.h"
-#include "helper.h"
-USING_NS_CC;
--- Classes/SkillKinds.h
+++
@@ -1,9 +0,0 @@
-#ifndef SKILLKINDS
-#define SKILLKINDS
-#include "cocos2d.h"
-#include "UnitClass.h"
-#include "GameScene.h"
-#include "skillClass.h"
-USING_NS_CC;
-void UsingFireBall(GameScene* main_scene, Unit* hero, Vec2 pos);
-#endif // SKILLKINDS
